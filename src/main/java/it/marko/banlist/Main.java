c83a5949cfa30339527e40004e1103b737c4ce41
rospino74 <rospino74gmail.com>
2020-04-16 09:26:07 +0200

    Miglioramento dell'error log
    Ora gli errori verranno stampati così:
    ```
    Breve descrizione dell'errore
    ===== [BanList] Inizio Report =====
    *Stacktrace Java*
    ===== Fine Report =====
    ```

--- src/main/java/it/marko/banlist/Main.java
+++ src/main/java/it/marko/banlist/Main.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020. MemoryOfLife
+ * Copyright (c) 2020 MemoryOfLife
  * This file (Main.java) and its related project (BanList) are governed by the Apache 2.0 license.
  * You may not use them except in compliance with the License which can be found at:
  * http://www.apache.org/licenses/LICENSE-2.0
@@ -8,13 +8,17 @@
 package it.marko.banlist;
 
 import com.sun.net.httpserver.HttpServer;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
+import org.yaml.snakeyaml.error.YAMLException;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.util.logging.Level;
 
 public class Main extends JavaPlugin {
+    public static final String PREFIX = ChatColor.DARK_GREEN + "" + ChatColor.BOLD + "[BanList] " + ChatColor.RESET;
     private static Main instance;
     private HttpServer server;
 
@@ -47,11 +51,21 @@ public class Main extends JavaPlugin {
             public void run() {
                 try {
                     buildHTTPServer(path, port);
-                } catch (IOException e) {
+                } catch (IOException | YAMLException e) {
+                    //stampo l'errore
+                    getLogger().log(Level.WARNING, PREFIX + "Errore nel creare il server");
+
+                    //fornisco una breve spiegazione se l'eccezione è un'instanza di YAMLException
+                    if (e instanceof YAMLException)
+                        getLogger().log(Level.WARNING, PREFIX + "Sembra che il file di configurazione non sia valido!");
+
+                    //stampo un blocco con lo stackTrace
+                    getLogger().log(Level.WARNING, ChatColor.RED + "" + ChatColor.BOLD + "===== " + PREFIX + ChatColor.RED + "" + ChatColor.BOLD + "Inizio Report =====");
                     e.printStackTrace();
+                    getLogger().log(Level.WARNING, ChatColor.RED + "" + ChatColor.BOLD + "===== Fine Report =====");
                 }
             }
-        }.run();
+        }.runTaskAsynchronously(this);
 
     }
 
