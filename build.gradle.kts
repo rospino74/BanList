6ade6af8284de1e8a7332c9f421a16b6772aef09
rospino74 <rospino74gmail.com>
2020-04-16 09:26:44 +0200

    Cambiata la versione a 1.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2020 MemoryOfLife
+ * This file (build.gradle.kts) and its related project (BanList) are governed by the Apache 2.0 license.
+ * You may not use them except in compliance with the License which can be found at:
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
@@ -18,7 +25,7 @@ buildscript {
 }
 
 group = "it.marko.banlist"
-version = "1.0-STABLE"
+version = "1.0.1-STABLE"
 
 apply(plugin = "com.github.johnrengelman.shadow")
 apply(plugin = "java")
