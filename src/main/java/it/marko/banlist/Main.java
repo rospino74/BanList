6c6fff97bba9f13b3ccfdc3d31b1d17be5bab57b
rospino74 <rospino74gmail.com>
2020-04-27 12:25:06 +0200

    Aggiunto il supporto al mute

--- src/main/java/it/marko/banlist/Main.java
+++ src/main/java/it/marko/banlist/Main.java
@@ -7,7 +7,10 @@
 
 package it.marko.banlist;
 
+import com.earth2me.essentials.Essentials;
 import com.sun.net.httpserver.HttpServer;
+import it.marko.banlist.handlers.BanRequestHandler;
+import it.marko.banlist.handlers.MuteRequestHandler;
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -26,6 +29,7 @@ public class Main extends JavaPlugin {
     public static final String PREFIX = ChatColor.DARK_GREEN + "" + ChatColor.BOLD + "[BanList] " + ChatColor.RESET;
     private static Main instance;
     private HttpServer server;
+    private boolean isMutedEnabled;
 
     @Override
     public void onDisable() {
@@ -42,34 +46,55 @@ public class Main extends JavaPlugin {
     public void onEnable() {
         super.onEnable();
 
-        //assegno l'instanza
+        //assegno l'istanza
         Main.instance = this;
 
         //salvo i config di default
         saveDefaultConfig();
+
+        //deve essere abilitato il mute?
+        Essentials e = (Essentials) getServer().getPluginManager().getPlugin("Essentials");
+        if (e == null) {
+            //avviso che essentials non è installato
+            getLogger().warning("Essentials non è installato! Non potrai vedere i player mutati");
+
+            //imposto la variabile
+            isMutedEnabled = false;
+        } else isMutedEnabled = getConfig().getBoolean("show.mute");
 
         //avvio il server in un runnable
         new BukkitRunnable() {
             @Override
             public void run() {
                 try {
-                    //prendo i dati
-                    String path = getConfig().getString("output.path");
-                    int port = getConfig().getInt("output.port");
-
                     //creo il server
-                    buildHTTPServer(path, port);
+                    int port = getConfig().getInt("output.port");
+                    buildHTTPServer(port);
+
+                    //prendo i dati per ban
+                    String ban_path = getConfig().getString("output.path.ban");
+                    //imposto l'Handler
+                    server.createContext(ban_path, new BanRequestHandler());
+
+                    //se attivo il mute lo carico
+                    if (isMutedEnabled) {
+                        String mute_path = getConfig().getString("output.path.mute");
+                        server.createContext(mute_path, new MuteRequestHandler());
+                    }
+
+                    //eseguo il server
+                    server.start();
                 } catch (IOException | YAMLException e) {
                     //stampo l'errore
-                    getLogger().log(Level.SEVERE, PREFIX + "Errore nel creare il server");
+                    getLogger().severe("Errore nel creare il server");
 
                     //fornisco una breve spiegazione se l'eccezione è un'instanza di YAMLException
                     if (e instanceof YAMLException)
-                        getLogger().log(Level.SEVERE, PREFIX + "Sembra che il file di configurazione non sia valido!");
+                        getLogger().severe("Sembra che il file di configurazione non sia valido!");
 
                     //stampo un blocco con lo stackTrace
                     getLogger().log(Level.SEVERE, ChatColor.RED + "" + ChatColor.BOLD + "===== " + PREFIX + ChatColor.RED + "" + ChatColor.BOLD + "Inizio Report =====", e);
-                    getLogger().log(Level.SEVERE, ChatColor.RED + "" + ChatColor.BOLD + "===== Fine Report =====");
+                    getLogger().severe( ChatColor.RED + "" + ChatColor.BOLD + "===== Fine Report =====");
                 }
             }
         }.runTaskAsynchronously(this);
@@ -77,29 +102,30 @@ public class Main extends JavaPlugin {
     }
 
     /**
-     * Crea un'instanza del server HTTP
-     * @param path Percorso da utilizzare per la creazione del server
-     * @param port Porta da utilizzare per la creazione del server
-     * @throws IOException Lancia una {@link IOException} se si verifica un errore nella creazione del server
+     * Crea un'istanza del server HTTP
      *
+     * @param port Porta da utilizzare per la creazione del server
+     * @return {@link HttpServer} il server appena creato
+     * @throws IOException Lancia una {@link IOException} se si verifica un errore nella creazione del server
      * @see #stopHTTPServer()
      */
-    private void buildHTTPServer(String path, int port) throws IOException {
+    private HttpServer buildHTTPServer(int port) throws IOException {
         server = HttpServer.create(new InetSocketAddress(port), 0);
-        server.createContext(path, new RequestHandler());
         server.setExecutor(null);
-        server.start();
+
+        //ritorno il server
+        return server;
     }
 
     /**
-     * Ferma il server creato con {@link #buildHTTPServer(String, int)}
+     * Ferma il server creato con {@link #buildHTTPServer(int)}
      */
     private void stopHTTPServer() {
         server.stop(0);
     }
 
     /**
-     * Usa questo medoto per ottenere l'istanza della classe {@link Main}
+     * Usa questo metodo per ottenere l'istanza della classe {@link Main}
      *
      * @return L'istanza corrente del plugin
      * @see JavaPlugin
