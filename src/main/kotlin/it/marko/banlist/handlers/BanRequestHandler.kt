0b8747f9f111f9ddb2635d11eb831defeeb3ea4c
rospino74 <rospino74gmail.com>
2020-04-28 11:44:57 +0200

    Spostato request handler

--- src/main/kotlin/it/marko/banlist/handlers/BanRequestHandler.kt
+++ src/main/kotlin/it/marko/banlist/handlers/BanRequestHandler.kt
@@ -1,29 +1,31 @@
 /*
  * Copyright (c) 2020 MemoryOfLife
- * This file (RequestHandler.kt) and its related project (BanList) are governed by the Apache 2.0 license.
+ * This file (BanRequestHandler.kt) and its related project (BanList) are governed by the Apache 2.0 license.
  * You may not use them except in compliance with the License which can be found at:
  * http://www.apache.org/licenses/LICENSE-2.0
  */
 
-package it.marko.banlist
+package it.marko.banlist.handlers
 
 import com.google.gson.JsonObject
 import com.sun.net.httpserver.HttpExchange
 import com.sun.net.httpserver.HttpHandler
+import it.marko.banlist.Main
+import it.marko.banlist.listers.BanLister
 import org.bukkit.BanList
 import java.io.OutputStream
 
 /**
- * Classe per gestire le richieste verso l'url definito in `output.path`
+ * Classe per gestire le richieste verso l'url definito in `output.path.ban`
  */
-internal class RequestHandler : HttpHandler {
+internal class BanRequestHandler : HttpHandler {
     override fun handle(exchange: HttpExchange?) {
         val banList = BanLister()
         val out = JsonObject()
 
         //mostrare i ban per ip?
-        val showIP = Main.getInstance().config.getBoolean("show.byIP", true)
-        val showNAME = Main.getInstance().config.getBoolean("show.byNAME", true)
+        val showIP = Main.getInstance().config.getBoolean("show.ban.byIP", true)
+        val showNAME = Main.getInstance().config.getBoolean("show.ban.byNAME", true)
 
         //json di ban per ip e nomi
         if (showNAME) {
